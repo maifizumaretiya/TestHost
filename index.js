884a65560b51ca6da6f01e9e6a0780fde0affff9
maifizumaretiya <91270833+maifizumaretiya@users.noreply.github.com>
2022-06-22 10:30:51 +0530

    static file hosted

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var router = require('./route')
 
-var app = express()
-app.use('/api',router);
-app.listen(process.env.PORT,()=>{
-    console.log("Server Started...");
+mongoose.connect("mongodb+srv://maifizumaretiya:maifiz9292@cluster0.u9ppw.mongodb.net/Student?retryWrites=true&w=majority").then(()=>{
+    console.log("DB Connected");
+
+    app = express();
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(express.json())
+    app.use('/api',router)
+
+    app.listen(process.env.POST,()=>{
+        console.log("Server Started");
+    })
+}).catch((err)=>{
+    console.log(err);
 })
