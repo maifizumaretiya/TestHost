7132fd6df7d0de15bd129778c19f308d50761339
maifizumaretiya <91270833+maifizumaretiya@users.noreply.github.com>
2022-06-22 11:01:09 +0530

    static file hosted

--- route.js
+++ route.js
@@ -4,9 +4,10 @@ var router = express.Router();
 // router.get("/",(req,res)=>{
 //     res.sendFile("index.html",{root:__dirname});
 // });
-module.exports = router
 
-router.get('./movies',async(req,res)=>{
+
+router.get('/movies',async(req,res)=>{
     const iMovie = await Movie.find();
     res.send(iMovie);
 })
+module.exports = router
