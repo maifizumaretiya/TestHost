fca108bf1a2cff6f529f01a17341929d6f38b4e5
maifizumaretiya <91270833+maifizumaretiya@users.noreply.github.com>
2022-06-25 18:34:32 +0530

    static file hosted

--- route.js
+++ route.js
@@ -10,4 +10,51 @@ router.get('/movies',async(req,res)=>{
     const iMovie = await Movie.find();
     res.send(iMovie);
 })
+router.post('/movies',async(req,res)=>{
+    const iMovie = new Movie({
+        name : req.body.name,
+        rating : req.body.rating
+    })
+    console.log(req.body.name);
+    await iMovie.save((err,msg)=>{
+        if(err){
+            res.send(err)
+        }else{
+            res.send(msg)
+        }
+    })
+})
+
+router.patch("/movies/:id",async(req,res)=>{
+    const iMovie = await Movie.findOne({_id:req.params.id});
+    iMovie.name = req.body.name;
+    iMovie.rating = req.body.rating;
+
+    await iMovie.save((err,msg)=>{
+        if(err){
+            res.status(500).json({
+                "err":err
+            })
+        }else{
+            res.status(200).json({
+                "message":msg
+            })
+        }
+    })
+})
+
+router.delete("/movies/:id",async(req,res)=>{
+    await Movie.deleteOne({_id:req.params.id},(err,msg)=>{
+        if(err){
+            res.status(500).json({
+                "err":err
+            })
+        }else{
+            res.status(200).json({
+                "message":msg
+            })
+        }
+    })
+})
+
 module.exports = router
